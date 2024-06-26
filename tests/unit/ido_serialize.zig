b603b1737ec14bc26b056b42d0ca90f4c947f08e
Christopher Morton <sonro@gmx.com>
2024-06-26 13:32:12 +0100

    style: clean serialize testing

--- tests/unit/ido_serialize.zig
+++ tests/unit/ido_serialize.zig
@@ -2,8 +2,9 @@ const std = @import("std");
 const testing = std.testing;
 const allocator = testing.allocator;
 const ido = @import("ido");
-const Format = ido.Format;
 const util = @import("test-util");
+const Task = ido.Task;
+const Format = ido.Format;
 
 const TODO_PREFIX = "TODO: ";
 const DONE_PREFIX = "DONE: ";
@@ -11,79 +12,56 @@ const DESC_SEPARATOR = "\n";
 const TASK_SUFFIX = "\n\n";
 
 test "tasklist empty" {
-    try checkTaskList(&.{});
+    try checkList(&.{});
 }
 
 test "tasklist single task" {
-    try checkTaskList(&.{
-        .{ .name = "foo", .description = null, .done = false },
+    try checkList(&.{
+        .{ .name = "foo" },
     });
 }
 
 test "tasklist multiple tasks" {
-    try checkTaskList(&.{
-        .{ .name = "foo", .description = null, .done = false },
-        .{ .name = "bar", .description = null, .done = true },
+    try checkList(&.{
+        .{
+            .name = "foo",
+        },
+        .{ .name = "bar", .done = true },
     });
 }
 
 test "simple task" {
-    try checkTodoSimple("foo");
+    try checkTask(.{ .name = "foo" });
 }
 
 test "simple done task" {
-    try checkDoneSimple("foo");
+    try checkTask(.{ .name = "foo", .done = true });
 }
 
 test "task with description" {
-    try checkTodo("foo", "bar");
+    try checkTask(.{ .name = "foo", .description = "bar" });
 }
 
 test "task with description containing newline" {
-    try checkTodo("foo", "bar\nbaz");
+    try checkTask(.{ .name = "foo", .description = "bar\nbaz" });
 }
 
 test "done task with description" {
-    try checkDone("foo", "bar");
+    try checkTask(.{ .name = "foo", .description = "bar", .done = true });
 }
 
-fn checkTodoSimple(comptime name: []const u8) !void {
-    const task = try ido.Task.newSimple(name);
-    const expected = comptime expectedTodoSimple(name);
-    try checkSerializeTask(expected, task);
+fn checkTask(comptime task: Task) !void {
+    const expected = expectedTaskString(task);
+    try testSerializeTask(expected, task);
 }
 
-fn checkTodo(comptime name: []const u8, comptime desc: []const u8) !void {
-    const task = try ido.Task.new(name, desc);
-    const expected = comptime expectedTodo(name, desc);
-    try checkSerializeTask(expected, task);
-}
-
-fn checkDoneSimple(comptime name: []const u8) !void {
-    var task = try ido.Task.newSimple(name);
-    task.done = true;
-    const expected = comptime expectedDoneSimple(name);
-    try checkSerializeTask(expected, task);
-}
-
-fn checkTaskList(comptime tasks: []const ido.Task) !void {
+fn checkList(comptime tasks: []const ido.Task) !void {
     const expected = try expectedListString(tasks);
     defer expected.deinit();
-    var string = try std.ArrayList(u8).initCapacity(allocator, expected.items.len);
-    defer string.deinit();
-    try Format.serializeTaskList(tasks, string.writer());
-    try testing.expectEqualStrings(expected.items, string.items);
+    try testSerializeTaskList(expected.items, tasks);
 }
 
-fn expectedListString(comptime tasks: []const ido.Task) !std.ArrayList(u8) {
-    var string = std.ArrayList(u8).init(allocator);
-    inline for (tasks) |task| {
-        try string.appendSlice(expectedTask(task));
-    }
-    return string;
-}
-
-fn expectedTask(comptime task: ido.Task) []const u8 {
+fn expectedTaskString(comptime task: ido.Task) []const u8 {
     if (task.done and task.description == null) {
         return expectedDoneSimple(task.name);
     } else if (task.done) {
@@ -95,18 +73,27 @@ fn expectedTask(comptime task: ido.Task) []const u8 {
     }
 }
 
-fn checkDone(comptime name: []const u8, comptime desc: []const u8) !void {
-    var task = try ido.Task.new(name, desc);
-    task.done = true;
-    const expected = comptime expectedDone(name, desc);
-    try checkSerializeTask(expected, task);
+fn expectedListString(comptime tasks: []const Task) !std.ArrayList(u8) {
+    var string = std.ArrayList(u8).init(allocator);
+    inline for (tasks) |task| {
+        try string.appendSlice(expectedTaskString(task));
+    }
+    return string;
 }
 
-fn checkSerializeTask(comptime expected: []const u8, task: ido.Task) !void {
-    var string = try std.ArrayList(u8).initCapacity(allocator, expected.len);
-    defer string.deinit();
-    try Format.serializeTask(task, string.writer());
-    try testing.expectEqualStrings(expected, string.items);
+fn testSerializeTask(expected: []const u8, task: Task) !void {
+    try testSerializeFn(expected, Format.serializeTask, task);
+}
+
+fn testSerializeTaskList(expected: []const u8, tasks: []const Task) !void {
+    try testSerializeFn(expected, Format.serializeTaskList, tasks);
+}
+
+fn testSerializeFn(expected: []const u8, testFn: anytype, taskArg: anytype) !void {
+    var actual = try std.ArrayList(u8).initCapacity(allocator, expected.len);
+    defer actual.deinit();
+    try testFn(taskArg, actual.writer());
+    try testing.expectEqualStrings(expected, actual.items);
 }
 
 fn expectedTodoSimple(comptime name: []const u8) []const u8 {
