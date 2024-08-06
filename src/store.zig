3203226216a4ef089459e468b05f55a2fec5e3b7
Christopher Morton <sonro@gmx.com>
2024-08-06 22:01:20 +0100

    refactor: store init into comptime function

--- src/store.zig
+++ src/store.zig
@@ -36,40 +36,37 @@ pub const TaskStore = struct {
     /// }
     /// ```
     pub fn init(ptr: anytype) TaskStore {
-        const T = @TypeOf(ptr);
+        const impl = Implentation(@TypeOf(ptr));
+
+        return .{
+            .ptr = ptr,
+            .vtable = &.{
+                .save = impl.save,
+                .load = impl.load,
+                .loadInto = impl.loadInto,
+            },
+        };
+    }
+
+    fn Implentation(comptime T: type) type {
         const ptr_info = @typeInfo(T);
 
         if (ptr_info != .Pointer) @compileError("ptr must be a pointer");
         if (ptr_info.Pointer.size != .One) @compileError("ptr must be a single item pointer");
 
-        const gen = comptime struct {
+        return struct {
             pub fn save(ctx: *anyopaque, tasks: []const Task) anyerror!void {
                 const self: T = @ptrCast(@alignCast(ctx));
                 return ptr_info.Pointer.child.save(self, tasks);
             }
-            pub fn load(
-                ctx: *anyopaque,
-                allocator: std.mem.Allocator,
-            ) anyerror!std.ArrayList(Task) {
+            pub fn load(ctx: *anyopaque, allocator: std.mem.Allocator) anyerror!std.ArrayList(Task) {
                 const self: T = @ptrCast(@alignCast(ctx));
                 return ptr_info.Pointer.child.load(self, allocator);
             }
-            pub fn loadInto(
-                ctx: *anyopaque,
-                tasklist: *std.ArrayList(Task),
-            ) anyerror!void {
+            pub fn loadInto(ctx: *anyopaque, tasklist: *std.ArrayList(Task)) anyerror!void {
                 const self: T = @ptrCast(@alignCast(ctx));
                 return ptr_info.Pointer.child.loadInto(self, tasklist);
             }
-        };
-
-        return .{
-            .ptr = ptr,
-            .vtable = &.{
-                .save = gen.save,
-                .load = gen.load,
-                .loadInto = gen.loadInto,
-            },
         };
     }
 };
