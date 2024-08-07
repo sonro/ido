26d5f14107a761335811051799ae93b8d2f40d96
Christopher Morton <sonro@gmx.com>
2024-08-07 12:53:33 +0100

    feat: basic cli exe

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,43 @@
 const std = @import("std");
 const ido = @import("ido");
 
-pub fn main() !void {}
+const usage = "Usage: zig ido -f <file.ido>";
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const args = try Args.init(arena.allocator());
+
+    var store = ido.FileStore(ido.Format).init(arena.allocator(), args.file);
+    var manager = try ido.Manager.init(arena.allocator(), store.taskStore());
+
+    try manager.load();
+
+    var stdout = std.io.bufferedWriter(std.io.getStdOut().writer());
+    try ido.Format.serializeTaskList(manager.allTasks(), stdout.writer());
+    try stdout.flush();
+}
+
+const Args = struct {
+    file: []const u8,
+
+    pub fn init(allocator: std.mem.Allocator) !Args {
+        const args = try std.process.argsAlloc(allocator);
+
+        if (args.len != 3 or !std.mem.eql(u8, args[1], "-f")) {
+            std.debug.print("{s}\n", .{usage});
+            return error.InvalidArgs;
+        }
+
+        return Args{
+            .file = args[2],
+        };
+    }
+
+    pub fn deinit(self: Args, allocator: std.mem.Allocator) void {
+        allocator.free(self.file);
+    }
+};
--- tests/integration/cli.zig
+++ tests/integration/cli.zig
@@ -1,3 +1,31 @@
 const std = @import("std");
 const testing = std.testing;
 const ExeRunner = @import("ExeRunner.zig");
+
+// relative to the tmp dir
+const binary = "../../../zig-out/bin/ido";
+const run_on_test_ido = .{ binary, "-f", "test.ido" };
+
+test "file not found" {
+    var runner = ExeRunner.init();
+    defer runner.deinit();
+    // don't add any files
+
+    var res = try runner.run(testing.allocator, run_on_test_ido);
+    defer res.deinit(testing.allocator);
+
+    try testing.expectEqual(ExeRunner.Result.Status.failure, res.status);
+}
+
+test "empty file" {
+    var runner = ExeRunner.init();
+    defer runner.deinit();
+
+    try runner.addFile("test.ido", "");
+    var res = try runner.run(testing.allocator, run_on_test_ido);
+    defer res.deinit(testing.allocator);
+
+    try testing.expectEqual(ExeRunner.Result.Status.success, res.status);
+    try testing.expectEqual(@as(usize, 0), res.stdout.len);
+    try testing.expectEqual(@as(usize, 0), res.stderr.len);
+}
